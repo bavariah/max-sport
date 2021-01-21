8dce693c8cf4373b31189d950ca08e2f0848a9f7
bavariah <48217024+bavariah@users.noreply.github.com>
2021-01-21 12:12:36 +0100

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,194 @@
+const menu = [
+  {
+    id: 1,
+    title: "sipka",
+    category: "sipke",
+    price: 150,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom
+    `,
+  },
+  {
+    id: 2,
+    title: "klupa",
+    category: "klupe",
+    price: 122,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 3,
+    title: "sipka sipka",
+    category: "sipke",
+    price: 133,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 4,
+    title: "sipka",
+    category: "sipke",
+    price: 134,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 5,
+    title: "klupa",
+    category: "klupe",
+    price: 144,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 6,
+    title: "sipka",
+    category: "sipke",
+    price: 111,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 7,
+    title: "sipka",
+    category: "sipke",
+    price: 123,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 8,
+    title: "klupa",
+    category: "klupe",
+    price: 234,
+    img: "../props/test.jpg",
+    desc: `o1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 9,
+    title: "bucice",
+    category: "bucice",
+    price: 122,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 10,
+    title: "bucice",
+    category: "bucice",
+    price: 155,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 11,
+    title: "klupa",
+    category: "klupe",
+    price: 123,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 12,
+    title: "klupa",
+    category: "bucice",
+    price: 155,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 13,
+    title: "klupa",
+    category: "bucice",
+    price: 166,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 14,
+    title: "klupa",
+    category: "klupe",
+    price: 162,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 15,
+    title: "bucice",
+    category: "bucice",
+    price: 14,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 16,
+    title: "bucice",
+    category: "bucice",
+    price: 12,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+  {
+    id: 17,
+    title: "klupa",
+    category: "klupe",
+    price: 12,
+    img: "../props/test.jpg",
+    desc: `1. Half Rack 2 PROFESIONALNI - 375 eura
+    2. Univerzalna bench klupa PROFESIONALNA - 170 eura
+    4. Bucice sa navojima 35cm duzine - 2000din/kom
+    5. Bucice sa navojima 38cm duzine - 2200din/kom`,
+  },
+];
+
 const menuToggle = document.querySelector('.toggle');
 const hero = document.querySelector('.hero');
 
@@ -5,3 +196,29 @@ const hero = document.querySelector('.hero');
         menuToggle.classList.toggle('active');
         hero.classList.toggle('active');
       })
+
+const sectionProduct = document.querySelector('.products-center');
+
+window.addEventListener('DOMContentLoaded', function() {
+  let displayMenu = menu.map(function(item){
+    // console.log(item);
+    return `<article class="product">
+    <div class="img-container">
+      <img
+        src=${item.img}
+        alt=${item.title}
+        class="product-img"
+      />
+      <p class="item-text">
+        ${item.desc}
+        <i class="las la-chevron-down"></i>
+      </p>
+    </div>
+    <h3>${item.title}</h3>
+    <h4>€ ${item.price}</h4>
+  </article>`;
+  });
+  displayMenu = displayMenu.join('');
+  sectionProduct.innerHTML = displayMenu
+});
+
