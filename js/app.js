a3ad2394dde8a62ac133841106480f394387ba66
bavariah <48217024+bavariah@users.noreply.github.com>
2021-01-22 14:46:04 +0100

    con

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toggle');
+const hero = document.querySelector('.hero');
+
+      menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        hero.classList.toggle('active');
+      })
