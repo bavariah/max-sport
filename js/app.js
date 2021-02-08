b1c3823cc27138a23fd5f240e95cc27bcabd95e9
bavariah <48217024+bavariah@users.noreply.github.com>
2021-02-08 14:44:13 +0100

    uslovi

--- js/app.js
+++ js/app.js
@@ -9,3 +9,23 @@ const hero = document.querySelector('.hero');
 function goBack() {
   window.history.go(-1);
 }
+
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
